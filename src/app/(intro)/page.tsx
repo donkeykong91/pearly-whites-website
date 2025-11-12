5b464561d3c1f4ff67104814aad1832f94113112
donkeykong91 <darylnator@gmail.com>
2025-11-12 14:08:06 -0800

    🚨Fixed prettier errors

--- src/app/(intro)/page.tsx
+++ src/app/(intro)/page.tsx
@@ -1,11 +1,11 @@
-import Details from '@/components/ui/Details'
+import Details from '@/components/ui/Details';
 
 const IntroPage = () => {
   return (
     <main className="flex justify-start self-center sm:col-[1/4] sm:row-[2/3] md:col-[2/8]">
       <Details />
     </main>
-  )
-}
+  );
+};
 
-export default IntroPage
+export default IntroPage;
