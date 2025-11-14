bef900a2728daa7432e5c76ed876e07fbc39b8ee
donkeykong91 <darylnator@gmail.com>
2025-11-14 11:22:22 -0800

    📱Updated main content for mobile

--- src/app/(intro)/page.tsx
+++ src/app/(intro)/page.tsx
@@ -13,7 +13,7 @@ const IntroPage = () => {
 
   return (
     <main
-      className={`transform-all flex justify-start self-center transition duration-700 ease-out sm:col-[1/4] sm:row-[2/3] md:col-[2/8] ${visible ? 'opacity-100 md:translate-y-0' : 'opacity-0 md:-translate-y-10'}`}
+      className={`transform-all flex transition duration-700 ease-out sm:col-[1/4] sm:row-[2/3] md:col-[2/8] md:self-center ${visible ? 'opacity-100 md:translate-y-0' : 'opacity-0 md:-translate-y-10'}`}
     >
       <Details />
     </main>
--- src/components/ui/Details.tsx
+++ src/components/ui/Details.tsx
@@ -9,7 +9,7 @@ const Details = () => {
   const DETAILS = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
   return (
-    <div className="sm:grid sm:grid-cols-4">
+    <div className="grid sm:grid-cols-4">
       <Card title={TITLE} details={DETAILS} />
     </div>
   );
