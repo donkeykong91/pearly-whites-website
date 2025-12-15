10643feffcbbc0f50e4677920630f11d172caad9
donkeykong91 <darylnator@gmail.com>
2025-12-15 05:24:15 -0800

    🐛Fixed timing issue for animation

--- src/app/(intro)/page.tsx
+++ src/app/(intro)/page.tsx
@@ -6,23 +6,32 @@ import { usePathname } from 'next/navigation';
 const IntroPage = () => {
   const [visible, setVisible] = useState(false);
   const [ready, setReady] = useState(false);
+  const [animate, setAnimate] = useState(false);
   const pathName = usePathname();
 
   // Slide-fade-in animation effect
   useLayoutEffect(() => {
-    if (pathName === '/') {
-      setReady(false);
-      setVisible(false);
-      requestAnimationFrame(() => {
-        setReady(true);
-        setVisible(true);
-      });
-    }
+    if (pathName !== '/') return;
+
+    setAnimate(false);
+    setReady(false);
+    setVisible(false);
+
+    const timeout = setTimeout(() => {
+      setAnimate(true);
+    }, 500);
+
+    requestAnimationFrame(() => {
+      setReady(true);
+      setVisible(true);
+    });
+
+    return () => clearTimeout(timeout);
   }, [pathName]);
 
   return (
     <div
-      className={`self-center ${visible ? 'opacity-100 md:translate-y-0' : 'opacity-0 md:-translate-y-10'} ${ready ? 'transition duration-700 ease-out' : 'transition-none'}`}
+      className={`self-center ${visible && animate ? 'opacity-100 md:translate-y-0' : 'opacity-0 md:-translate-y-10'} ${ready ? 'transition duration-700 ease-out' : 'transition-none'}`}
     >
       <Details />
     </div>
